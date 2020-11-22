1bc085e0f715a523dff8c8690dc3bb04f5ff4ce8
unknown <1456421562@qq.com>
2020-11-22 23:10:46 +0800

    ===

---
+++ lesson5-2.js
@@ -0,0 +1,15 @@
+const Gen= (time)=>{
+  return new Promise((resolve, reject) => {
+    setTimeout(function () {
+      if(time < 0.5){
+        reject(time)
+      }else{
+        resolve(time)
+      }
+    }, time)
+  })
+}
+Gen(Math.random()*1000 )
+.then(val => console.log('resolve', val))
+.catch(err => console.log('reject', err))
+.finally(()=> { console.log('finish') })
